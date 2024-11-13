14bccdbeec66f937d761946a524b677c26055dca
Saharsh Agrawal <71719364+saharsh-agrawal@users.noreply.github.com>
2024-11-13 10:02:15 +0530

    jwt accessed and user model created

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,43 @@
 import Link from "next/link";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import User from "@/app/models/User";
+import { connectToDatabase } from "@/app/lib/mongodb";
 
-const Home = () => {
+const Home = async () => {
+	const cookieStore = await cookies();
+	const cookie = cookieStore.get("heimdall");
+
+	if (!cookie) {
+		redirect(
+			"https://heimdall.metakgp.org/?redirect_url=https://travel.metakgp.org/"
+		);
+	}
+	const jwt = cookie.value;
+
+	// decode jwt
+	const email = JSON.parse(atob(jwt.split(".")[1])).email; // get the user email from jwt
+
+	await connectToDatabase();
+
+	// const newUser = new User({
+	// 	email: email,
+	// 	name: "Saharsh Agrawal",
+	// 	roll: "21IM3EP27",
+	// 	number: "1234567890",
+	// });
+	// await newUser.save();
+
+	const user = await User.findOne({
+		email: email,
+	});
+	if (!user) {
+		return "Sign Up required";
+	}
+
 	return (
 		<div className="container text-center mt-5">
-			<h1 className="mb-4">Welcome to Travel Buddy</h1>
+			<h1 className="mb-4">Welcome to Travel Buddy - {user.name}</h1>
 			<h3>Find your travel partners for your next trip</h3>
 			<br />
 			<h6>
@@ -22,9 +56,9 @@ const Home = () => {
 			<footer className="mt-5">
 				<p>
 					Please note that this application is made to help KGPians
-					with similiar travel plans. Any data submitted here
-					will be visible to others who input a similiar time slot.
-					Please consider before submitting any personal information.
+					with similiar travel plans. Any data submitted here will be
+					visible to others who input a similiar time slot. Please
+					consider before submitting any personal information.
 				</p>
 			</footer>
 		</div>
