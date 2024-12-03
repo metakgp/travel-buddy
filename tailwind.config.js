dcb111bb93f8774e7a579214214696c172136938
Adi-204 <adiboghawala@gmail.com>
2024-12-04 00:05:45 +0530

    Removed Flowbite dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
@@ -14,7 +13,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
 };
