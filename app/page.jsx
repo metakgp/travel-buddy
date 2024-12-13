c74c50cd1c4d3412a2dc7362f870a1351ae350b1
Saharsh Agrawal <71719364+saharsh-agrawal@users.noreply.github.com>
2024-12-14 01:26:49 +0530

    slight label correction

--- app/page.jsx
+++ app/page.jsx
@@ -7,15 +7,31 @@ const Home = () => {
 			<h3>Find your travel partners for your next trip or train ride</h3>
 			<br />
 			<div className="d-flex justify-content-center gap-3">
-			<Link href="/trips" className="btn btn-lg text-white bg-gradient-to-br from-[#D2691E] to-[#8e5337] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-yellow-500 font-medium rounded-full text-sm px-3 py-2 text-center transition-all duration-500"
-				><div className="gap-2 flex flex-row">
-				<img src='/assets/trips.png' alt="Trip Icon" className="w-8 h-8" />
-				<span> Travel Plans </span></div>
+				<Link
+					href="/trips"
+					className="btn btn-lg text-white bg-gradient-to-br from-[#D2691E] to-[#8e5337] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-yellow-500 font-medium rounded-full text-sm px-3 py-2 text-center transition-all duration-500"
+				>
+					<div className="gap-2 flex flex-row">
+						<img
+							src="/assets/trips.png"
+							alt="Trip Icon"
+							className="w-8 h-8"
+						/>
+						<span>Road Trips</span>
+					</div>
 				</Link>
-				<Link href="/trains" className="btn btn-lg text-white bg-gradient-to-br from-[#D2691E] to-[#8e5337] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-yellow-500 font-medium rounded-full text-sm px-3 py-2 text-center transition-all duration-500"
-				><div className="gap-2 flex flex-row">
-				<img src='/assets/train.png' alt="Trip Icon" className="w-8 h-8" />
-				<span> Train Journeys</span></div>
+				<Link
+					href="/trains"
+					className="btn btn-lg text-white bg-gradient-to-br from-[#D2691E] to-[#8e5337] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-yellow-500 font-medium rounded-full text-sm px-3 py-2 text-center transition-all duration-500"
+				>
+					<div className="gap-2 flex flex-row">
+						<img
+							src="/assets/train.png"
+							alt="Trip Icon"
+							className="w-8 h-8"
+						/>
+						<span>Train Journeys</span>
+					</div>
 				</Link>
 			</div>
 			<footer className="mt-5">
@@ -25,10 +41,17 @@ const Home = () => {
 					visible to others who input a similiar time slot. Please
 					consider before submitting any personal information.
 				</p>
-				<Link href="https://github.com/metakgp/travel-buddy" className="btn btn-lg text-white bg-gradient-to-br from-[#D2691E] to-[#8e5337]
-				 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none  focus:ring-yellow-500 font-medium rounded-full text-sm px-3 py-2 text-center transition-all duration-500">
+				<Link
+					href="https://github.com/metakgp/travel-buddy"
+					className="btn btn-lg text-white bg-gradient-to-br from-[#D2691E] to-[#8e5337]
+				 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none  focus:ring-yellow-500 font-medium rounded-full text-sm px-3 py-2 text-center transition-all duration-500"
+				>
 					<div className="gap-3 flex flex-row">
-						<img src='/assets/user.png' alt="Trip Icon" className="w-8 h-8" />
+						<img
+							src="/assets/user.png"
+							alt="Trip Icon"
+							className="w-8 h-8"
+						/>
 						<span> Contribute to the project </span>
 					</div>
 				</Link>
